b26e69898bff841cd0cef3076bddf62910182614
Alvin <alvinangjh@gmail.com>
2020-11-09 22:22:18 +0800

    table load

--- php/db/table_load.sql
+++ php/db/table_load.sql
@@ -30,7 +30,7 @@
     `startTime` TIME NOT NULL,
     `endTime` TIME NOT NULL,
     `activityDate` DATE NOT NULL,
-    `itineraryID` integer NOT NULL,
+    `itineraryID` integer NOT NULL
     ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
        CREATE TABLE if not exists `custom_loc` (
@@ -62,11 +62,7 @@
     ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 
-    -- insert into user values ("1", "jerreal",'luu', 'Bedok North Street 5 Blk 3018 Singapore 486132', "jerrealluu@freshie.snatcher", "iluvxmm", "10/04/1995");
-
-    -- insert into user values ("2", "elvin",'ung', '9 Changi South Street 3 #07-05 FREIGHT LINK EXPRESS DISTRCENTRE Singapore 486361', "elvinpresley@careyme.pls", "abc123", "09/04/1996");
-
-    -- insert into user values ("3", "jairald",'liong', '320 SERANGOON ROAD #04-55 SERANGOON PLAZA Singapore 218108', "jairald@aphro.dite", "ijustneed1chance", "11/04/1997");
+    
 
 
 insert into itinerary values (1,'Down the rabbit hole', '2020/10/19', "2020/10/20",'Nature', "1", 0);
@@ -74,6 +70,6 @@ insert into itinerary values (2,'Ready, STEADY, Go!', '2020/10/19', "2020/10/20"
 insert into itinerary values (3,"Let's go down for coffee", '2020/10/19', "2020/10/20",'Casual', "2", 4);
 insert into itinerary values (4,'Make Singapore great again', '2020/08/19', "2020/08/20",'Family', "3", 16);
 
-    insert into activity values (1, '002a031ff1aa6b9471e8327b48fb5e2014a'); 
-    insert into activity values (2, '0026271c23371bb4aafbf5985bef950172e'); 
-    insert into activity values (3, '002f1eb8d5a13324c56b25344b30465d861'); 
+    -- insert into activity values (1, '002a031ff1aa6b9471e8327b48fb5e2014a'); 
+    -- insert into activity values (2, '0026271c23371bb4aafbf5985bef950172e'); 
+    -- insert into activity values (3, '002f1eb8d5a13324c56b25344b30465d861'); 
